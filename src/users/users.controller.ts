6616651d3f279eb6fc32b760747be041ffa798ca
Kelly Natalia <kellynatalia_toro16@utp.edu.co>
2025-10-06 10:14:42 -0500

    Actualización CRUD

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { IUser } from 'src/interfaces';
 
 @Controller('users')
 export class UsersController {
@@ -13,7 +14,20 @@ export class UsersController {
     @Get(':id')
     findOne(@Param('id') id: number){
         return this.usersService.findOne(Number(id));
-
     }
 
+    @Post()
+    create(@Body() body: Omit<IUser, 'id'>){
+       return this.usersService.create(body);
+    }
+
+   @Put(':id') 
+    UpdateDateColumn(@Param('id') id: string, @Body() body: Omit<IUser, 'id'>){
+        return this.usersService.update(Number(id), body)   
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string){
+        return this.usersService.remove(Number(id));
+    }
 } 
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import {IProducts, IUser} from 'src/interfaces';
 
-export type IUser = {
-    id: number,
-    name: string,
-    email: string,           
-}
 @Injectable()
 export class UsersService {
 
@@ -12,6 +8,7 @@ export class UsersService {
         {id: 1, name: 'Elena', email: 'elena@gamil.com'},
         {id: 2, name: 'Jefferson', email: 'Pulido@gmail.com'}
     ]
+
     findAll(): IUser[] {
         return this.users
     }
@@ -20,5 +17,30 @@ export class UsersService {
         const userFind = this.users.find((user) => user.id === id)
         if (!userFind) throw new NotFoundException('usuario no encontrado')
         return userFind
+    }
+
+     create(user: Omit<IUser, 'id'>): IUser {
+        const newId = this.users.length > 0 
+            ? this.users[this.users.length - 1].id + 1
+            : 1;
+
+        const newUser: IUser = {
+            id: newId, ...user
+        };
+
+        this.users.push(newUser);
+        return newUser;
+    }   
+
+    update(id:number, newUser: Omit<IUser, 'id'>): IUser {
+       const user = this.findOne(id);
+       Object.assign(user, newUser);
+       return user;
+    }
+
+    remove(id: number){
+        const user = this.users.findIndex((user) => user.id === id);
+        this.users.splice(user, 1);
+        return { delete: true } 
     }
 }
